cb91d9cd2c90d88a03fc06eaf2bd2980086b6810
nave <nave1369@gmail.com>
2021-12-26 15:10:43 +0200

    add multy browser support

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,4 +1,9 @@
 import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+import utilities.common_ops
 
 driver = None
 action = None
@@ -7,6 +12,11 @@ action = None
 @pytest.fixture(scope='class')
 def init_web(request):
     driver =None
+    platform: str = utilities.common_ops.get_data("BrowserType")
+    if platform.lower() == 'chrome':
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif platform.lower() == 'firefox':
+        driver = webdriver.Firefox(GeckoDriverManager().install())
     globals()['driver'] = driver
     request.cls.driver = driver
     yield
